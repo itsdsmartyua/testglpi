b08d48b4d61dfc583fb865a778a9275dd9f94962
itsdsmartyua <sdsmartyua@gmail.com>
2026-01-12 13:39:58 +0200

    Update hook.php

--- hook.php
+++ hook.php
@@ -5,10 +5,7 @@ if (!defined('GLPI_ROOT')) {
    die("Sorry. You can't access this file directly");
 }
 
-/**
- * GLPI 11: NotificationSetting moved to Glpi\Notification\NotificationSetting
- * Need alias so our setting class "extends NotificationSetting" and is instance of it.
- */
+// GLPI 11: NotificationSetting moved to Glpi\Notification\NotificationSetting
 if (!class_exists('NotificationSetting') && class_exists('Glpi\\Notification\\NotificationSetting')) {
    class_alias('Glpi\\Notification\\NotificationSetting', 'NotificationSetting');
 }
@@ -19,10 +16,10 @@ function plugin_init_telegrambot(): void
 
    $PLUGIN_HOOKS['csrf_compliant']['telegrambot'] = true;
 
-   // Config page (Notification Setting UI)
+   // Config page (we expose settings UI from plugin menu)
    $PLUGIN_HOOKS['config_page']['telegrambot'] = 'front/notificationwebsocketsetting.form.php';
 
-   // Legacy/required classes
+   // Required classes
    require_once __DIR__ . '/inc/bot.class.php';
    require_once __DIR__ . '/inc/fields.class.php';
    require_once __DIR__ . '/inc/cron.class.php';
@@ -32,33 +29,12 @@ function plugin_init_telegrambot(): void
 
    if ((new Plugin())->isActivated('telegrambot')) {
 
-      // 1) Register notification mode (channel) in standard GLPI notifications
+      // Register Telegram as notification mode (standard templates)
       Notification_NotificationTemplate::registerMode(
          'telegram',
          __('Telegram', 'telegrambot'),
          'telegrambot'
       );
 
-      // 2) Register settings handler for this mode (critical for UI inside Notifications)
-      NotificationSettingConfig::register(
-         'telegram',
-         PluginTelegrambotNotificationWebsocketSetting::class
-      );
-
       // Cron
       $PLUGIN_HOOKS['cron']['telegrambot'] = [
-         'cronMessagelistener' => [
-            'function'   => 'plugin_telegrambot_cronMessagelistener',
-            'frequency'  => MINUTE_TIMESTAMP, // recommended: 60s
-            'mode'       => CronTask::MODE_EXTERNAL
-         ]
-      ];
-
-      // lowercase wrapper (GLPI sometimes calls lowercase)
-      $PLUGIN_HOOKS['cron']['telegrambot']['cronmessagelistener'] = [
-         'function'   => 'plugin_telegrambot_cronmessagelistener',
-         'frequency'  => MINUTE_TIMESTAMP,
-         'mode'       => CronTask::MODE_EXTERNAL
-      ];
-   }
-}
