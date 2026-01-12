731cb536c3bb398c9ec29aa3bc334f2b5299c8dc
itsdsmartyua <sdsmartyua@gmail.com>
2026-01-12 21:04:17 +0200

    Update notificationwebsocketsetting.form.php

--- front/notificationwebsocketsetting.form.php
+++ front/notificationwebsocketsetting.form.php
@@ -5,20 +5,25 @@ include_once __DIR__ . '/../../../inc/includes.php';
 
 global $CFG_GLPI;
 
+// Rights
 Session::checkRight('config', READ);
 
 require_once __DIR__ . '/../inc/notificationwebsocketsetting.class.php';
 
+$setting = new PluginTelegrambotNotificationWebsocketSetting();
+
+// POST save
 if (isset($_POST['update'])) {
    Session::checkRight('config', UPDATE);
-   $s = new PluginTelegrambotNotificationWebsocketSetting();
-   $s->showFormConfig([]);
+   $setting->postForm($_POST);
+
    Html::redirect($CFG_GLPI['root_doc'] . '/plugins/telegrambot/front/notificationwebsocketsetting.form.php');
    exit;
 }
 
 Html::header(__('Telegram', 'telegrambot'), $_SERVER['PHP_SELF'], 'config', 'notification');
 
-PluginTelegrambotNotificationWebsocketSetting::showFormConfig([]);
+// IMPORTANT: call NON-static method via object
+$setting->showFormConfig([]);
 
 Html::footer();
